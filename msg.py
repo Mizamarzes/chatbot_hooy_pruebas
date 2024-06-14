927239295892f2420884f8e4c2a41d59f00f2348
mizamarzes <mizamarzes@gmail.com>
2024-06-14 13:35:55 -0500

    emojis y tildes

--- msg.py
+++ msg.py
@@ -1,14 +1,15 @@
 import webbrowser
 import pyautogui
+import pyperclip
 from time import sleep
 
 # Lista de números de teléfono
 numbers = ['+573108520464', '+573157645002', '+573187528853']
-message = '🔈¡Atención usuarios de Hooy app! Hoy, domicilios a solo $1.000 pesos en Hooy app, Aprovechen y compren nuestras promos exclusivas por HOOY APP💜 🚀' 
+message = '🔈¡Atención usuarios de Hooy app! Hoy, domicilios a solo $1.000 pesos en Hooy app, Aprovechen y compren nuestras promos exclusivas por HOOY APP💜 🚀'
 
 # Abre WhatsApp Web una sola vez
 webbrowser.open('https://web.whatsapp.com')
-sleep(5)  # Espera suficiente tiempo para escanear el código QR
+sleep(10)  # Espera suficiente tiempo para escanear el código QR y que se cargue completamente
 
 for number in numbers:
     # Navega a la URL de chat específica
@@ -18,19 +19,14 @@ for number in numbers:
     pyautogui.press('enter')
     sleep(10)  # Espera a que la página cargue el chat
 
-    # Clic en el icono de adjuntar
-    pyautogui.click(1250, 980)  # Ajusta las coordenadas según tu pantalla
-    sleep(2)
-    
-    # Clic en el icono de adjuntar imágenes/fotos
-    pyautogui.click(1150, 780)  # Ajusta las coordenadas según tu pantalla
-    sleep(2)
+    # Escribir el mensaje
+    pyperclip.copy(message)
+    sleep(1)  # Espera un poco para asegurar que el mensaje esté escrito
 
-    # Escribir la ruta de la imagen en el diálogo de selección de archivos
-    pyautogui.typewrite(message)
-    pyautogui.press('enter')
-    sleep(2)  # Espera a que la imagen se cargue en el chat
+    # Pegar el mensaje desde el portapapeles
+    pyautogui.hotkey('ctrl', 'v')
+    sleep(2)  # Espera un poco para asegurar que el mensaje esté pegado
 
-    # Enviar la imagen
+    # Presiona Enter para enviar el mensaje
     pyautogui.press('enter')
-    sleep(2)  # Espera a que la imagen se envíe antes de proceder al siguiente número
+    sleep(5)  # Espera a que el mensaje se envíe antes de proceder al siguiente número
